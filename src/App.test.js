94712f362559f0bd8cecd6fd8dab8fbfaaeca891
devamanx <117819643+devamanx@users.noreply.github.com>
2023-01-11 19:03:35 +0530

    Unit Tests for Form Validation and Submission

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, act } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-// import App from './App';
 import Bookingpage from "./pages/Bookingpage";
-import { fetchAPI, submitAPI } from "./api/api";
+import { fetchAPI} from "./api/api";
+
 
 describe("Reserve a table functionality", () => {
   test("initializeTimes works properly", () => {
@@ -18,19 +18,43 @@ describe("Reserve a table functionality", () => {
   });
 
   test("updateTimes works properly", () => {
-    const {queryByText, getByRole, debug} = render(
+    const {queryByText, getByLabelText, debug} = render(
       <MemoryRouter>
         <Bookingpage />
       </MemoryRouter>
     )
     const todaysAvailableTimesExpected = fetchAPI(new Date()).join(", ");
-    const datePickerElement = getByRole('datepicker')
-    fireEvent.input(datePickerElement, {target:{value: '2023-01-31'}})
+    const chooseDateElement = getByLabelText("Choose date")
+    act(() => fireEvent.input(chooseDateElement, {target:{value: '2023-01-31'}}))
     const availableTimesElement = queryByText(todaysAvailableTimesExpected)
     expect(availableTimesElement).not.toBeInTheDocument();
 
   })
-  test("Form Validations", () => {
-    
+  test("Form Validations",async () => {
+    const {getByLabelText, findByTestId, getAllByTestId , debug} = render(<MemoryRouter><Bookingpage/></MemoryRouter>)
+    const chooseDateElement = getByLabelText("Choose date")
+    const chooseTimeElement = getByLabelText("Choose time")
+    const guestsElement = getByLabelText("Number of guests")
+    const occasionElement = getByLabelText("Occasion")
+
+    act(() => {
+      fireEvent.input(chooseDateElement, {target:{value: '2022-12-01'}});
+      fireEvent.blur(chooseDateElement);
+      fireEvent.input(chooseTimeElement, {target:{value: ''}})
+      fireEvent.blur(chooseTimeElement);
+      fireEvent.input(guestsElement, {target:{value:'50'}})
+      fireEvent.blur(guestsElement);
+      fireEvent.input(occasionElement, {target:{value:''}})
+      fireEvent.blur(occasionElement);
+    })
+    const dateValidationWarning = await findByTestId("date")
+    const timeValidationWarning = await findByTestId("time")
+    const guestsValidationWarning = await findByTestId("guests")
+    const occasionOptions =  getAllByTestId("occasion")
+
+    expect(dateValidationWarning).toBeInTheDocument()
+    expect(timeValidationWarning).toBeInTheDocument()
+    expect(occasionOptions.length).toBe(3)
+    expect(guestsValidationWarning).toBeInTheDocument()
   })
 });
